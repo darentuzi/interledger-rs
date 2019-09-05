6807b9da3306a249d3ab67317cb0035e7225e480
Georgios Konstantopoulos <me@gakonst.com>
2019-09-05 17:55:17 +0300

    ccp: merge IldcpAccount into Account

--- crates/interledger-ildcp/src/lib.rs
+++ crates/interledger-ildcp/src/lib.rs
@@ -4,9 +4,6 @@
 //!
 //! This is used by clients to query for their ILP address and asset details such as asset code and scale.
 
-use interledger_packet::Address;
-use interledger_service::Account;
-
 mod client;
 mod packet;
 mod server;
@@ -14,9 +11,3 @@ mod server;
 pub use client::get_ildcp_info;
 pub use packet::*;
 pub use server::IldcpService;
-
-pub trait IldcpAccount: Account {
-    fn client_address(&self) -> &Address;
-    fn asset_scale(&self) -> u8;
-    fn asset_code(&self) -> &str;
-}
